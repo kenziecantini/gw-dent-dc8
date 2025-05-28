7e6c4dc6346313a8cbb19815050de1264bc518ba
kenziecantini <macksiff@gmail.com>
2025-05-28 13:33:58 -0500

    committ

--- Week-8/app/auth.py
+++ Week-8/app/auth.py
@@ -67,6 +67,3 @@ def auth_callback():
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- Week-8/app/main.py
+++ Week-8/app/main.py
@@ -16,4 +16,4 @@ def protected():
     return jsonify({"msg": "Protected resource accessed!", "user": user})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
